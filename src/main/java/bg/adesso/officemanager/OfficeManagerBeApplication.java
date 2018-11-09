dbfdd4c60812c00e36c999783b711e057a712afb
tge0rgieva <tanya.georgieva@hotmail.com>
2018-11-09 16:09:40 +0200

    OfficeManager initial commit

---
+++ src/main/java/bg/adesso/officemanager/OfficeManagerBeApplication.java
@@ -0,0 +1,12 @@
+package bg.adesso.officemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeManagerBeApplication.class, args);
+	}
+}
